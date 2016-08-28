2bba8c9f34e480b6d53368515bbe35dd9dba8240
Merlin Reichwald <merlin.reichwald@gmail.com>
2016-08-28 10:47:32 +0200

    fpigjpdfgjdplö

--- src/main/java/de/nordakademie/eta/ui/EtaSession.java
+++ src/main/java/de/nordakademie/eta/ui/EtaSession.java
@@ -3,9 +3,26 @@ package de.nordakademie.eta.ui;
 import org.apache.wicket.protocol.http.WebSession;
 import org.apache.wicket.request.Request;
 
+import de.nordakademie.eta.users.User;
+import de.nordakademie.eta.users.Users;
+
 public class EtaSession extends WebSession {
 
-    public EtaSession(final Request request) {
-        super(request);
-    }
+	User user;
+
+	public boolean isLoggedIn() {
+		return user != null;
+	}
+
+	public void logOut() {
+		user = null;
+	}
+
+	public void logIn(String email, String pass) {
+		this.user = Users.byEmail(email);
+	}
+
+	public EtaSession(final Request request) {
+		super(request);
+	}
 }
