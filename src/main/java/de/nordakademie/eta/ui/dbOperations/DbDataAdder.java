428ec09c854771131c9ddf6fb4adc56bb91b3861
Nils <merlin.reichwald@gmail.com>
2016-08-28 13:55:07 +0200

    kj

---
+++ src/main/java/de/nordakademie/eta/ui/dbOperations/DbDataAdder.java
@@ -0,0 +1,15 @@
+package de.nordakademie.eta.ui.dbOperations;
+
+import org.apache.wicket.markup.html.panel.Panel;
+
+/**
+ * Created by nils on 28.08.2016.
+ */
+public class DbDataAdder extends Panel{
+
+    public DbDataAdder(String id) {
+        super(id);
+
+    }
+
+}
